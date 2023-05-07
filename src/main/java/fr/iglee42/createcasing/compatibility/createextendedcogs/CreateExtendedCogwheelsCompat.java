8770f69c622c3444408f325d187995dcac7ada3e
iglee <igleev2@gmail.com>
2023-05-07 11:26:41 +0200

    Fix : Mixins Crashs without extended cogs & Materials errors

--- src/main/java/fr/iglee42/createcasing/compatibility/createextendedcogs/CreateExtendedCogwheelsCompat.java
+++ src/main/java/fr/iglee42/createcasing/compatibility/createextendedcogs/CreateExtendedCogwheelsCompat.java
@@ -7,22 +7,36 @@ import com.rabbitminers.extendedgears.cogwheels.HalfShaftCogwheelBlock;
 import com.rabbitminers.extendedgears.cogwheels.ShaftlessCogwheelBlock;
 import com.simibubi.create.Create;
 import com.simibubi.create.content.contraptions.base.CasingBlock;
+import com.simibubi.create.content.contraptions.base.IRotate;
+import com.simibubi.create.content.contraptions.base.KineticTileEntity;
+import com.simibubi.create.content.contraptions.relays.elementary.CogWheelBlock;
+import com.simibubi.create.content.contraptions.relays.encased.EncasedCogwheelBlock;
 import com.simibubi.create.foundation.data.CreateRegistrate;
+import com.simibubi.create.foundation.utility.Iterate;
 import com.tterrag.registrate.util.entry.BlockEntityEntry;
 import com.tterrag.registrate.util.entry.BlockEntry;
 import com.tterrag.registrate.util.nullness.NonNullSupplier;
+import fr.iglee42.createcasing.CreateCasing;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.MaterialColor;
 import net.minecraftforge.fml.ModList;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 
+import static com.simibubi.create.content.contraptions.base.RotatedPillarKineticBlock.AXIS;
 import static com.simibubi.create.foundation.data.TagGen.axeOrPickaxe;
 
 public class CreateExtendedCogwheelsCompat {
@@ -78,5 +92,59 @@ public class CreateExtendedCogwheelsCompat {
         List<ResourceLocation> cogwheels = new ArrayList<>();
         ExtendedCogwheels.registrate().getAll(Registry.BLOCK_REGISTRY).stream().filter(b->b.getId().getPath().endsWith("_cogwheel") && !b.getId().getPath().contains("shaftless_") && !b.getId().getPath().contains("half_shaft_")).forEach(b-> cogwheels.add(b.getId()));
         cogwheels.stream().filter(r->ExtendedCogwheels.registrate().get(r.getPath(),Registry.BLOCK_REGISTRY)!= null).forEach(forEach);
+    }
+
+    public static boolean checkCogs(Level world, BlockState state, BlockPos pos, ItemStack heldItem) {
+        AtomicBoolean out = new AtomicBoolean(false);
+        List<CustomCogwheelCompat> cogs = new ArrayList<>();
+        ForgeRegistries.BLOCKS.getKeys().stream().filter(r -> ForgeRegistries.BLOCKS.getValue(r) instanceof CustomCogwheelCompat).forEach(r -> cogs.add((CustomCogwheelCompat) ForgeRegistries.BLOCKS.getValue(r)));
+        cogs.stream().filter(c->(c.getCogwheel() == state.getBlock() || c.getHalfCog() == state.getBlock() || c.getShaftlessCog() == state.getBlock()) && ((CogWheelBlock)state.getBlock()).isLargeCog() == c.isLargeCog() && c.getCasing().isIn(heldItem)).findFirst().ifPresent(encasedCog->{
+            if (world.isClientSide) {
+               out.set(true);
+               return;
+            }
+
+            BlockState encasedState = encasedCog.defaultBlockState().setValue(AXIS, state.getValue(AXIS));
+            Direction[] var14 = Iterate.directionsInAxis(state.getValue(AXIS));
+
+            for (Direction d : var14) {
+                BlockState adjacentState = world.getBlockState(pos.relative(d));
+                if (adjacentState.getBlock() instanceof IRotate) {
+                    IRotate def = (IRotate) adjacentState.getBlock();
+                    if (def.hasShaftTowards(world, pos.relative(d), adjacentState, d.getOpposite())) {
+                        encasedState = encasedState.cycle(d.getAxisDirection() == Direction.AxisDirection.POSITIVE ? EncasedCogwheelBlock.TOP_SHAFT : EncasedCogwheelBlock.BOTTOM_SHAFT);
+                    }
+                }
+            }
+
+            KineticTileEntity.switchToBlockState(world, pos, encasedState);
+            out.set(true);
+        });
+        if (CreateCasing.isCrystalClearLoaded()){
+            //if (CreateCrystalClearCompatibility.checkCustomCogs()) cir.setReturnValue(InteractionResult.SUCCESS);
+                /*List<CustomGlassCogwheelCompat> glassCogs = new ArrayList<>();
+                ForgeRegistries.BLOCKS.getKeys().stream().filter(r -> ForgeRegistries.BLOCKS.getValue(r) instanceof CustomGlassCogwheelCompat).forEach(r -> glassCogs.add((CustomGlassCogwheelCompat) ForgeRegistries.BLOCKS.getValue(r)));
+                glassCogs.stream().filter(c->c.getCogwheel() == state.getBlock() && ((MetalCogWheel)state.getBlock()).isLargeCog() == c.isLargeCog() && c.getCasing().isIn(heldItem)).findFirst().ifPresent(encasedCog->{
+                    if (world.isClientSide) {
+                        cir.setReturnValue(InteractionResult.SUCCESS);
+                    }
+
+                    BlockState encasedState = encasedCog.defaultBlockState().setValue(AXIS, state.getValue(AXIS));
+                    Direction[] var14 = Iterate.directionsInAxis(state.getValue(AXIS));
+
+                    for (Direction d : var14) {
+                        BlockState adjacentState = world.getBlockState(pos.relative(d));
+                        if (adjacentState.getBlock() instanceof IRotate) {
+                            IRotate def = (IRotate) adjacentState.getBlock();
+                            if (def.hasShaftTowards(world, pos.relative(d), adjacentState, d.getOpposite())) {
+                                encasedState = encasedState.cycle(d.getAxisDirection() == Direction.AxisDirection.POSITIVE ? EncasedCogwheelBlock.TOP_SHAFT : EncasedCogwheelBlock.BOTTOM_SHAFT);
+                            }
+                        }
+                    }
+
+                    KineticTileEntity.switchToBlockState(world, pos, encasedState);
+                    cir.setReturnValue(InteractionResult.SUCCESS);*/
+        }
+        return out.get();
     }
 }
