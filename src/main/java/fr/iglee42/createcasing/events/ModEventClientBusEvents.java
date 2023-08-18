569f88d2877eabcda23983149024b13162b5f5ad
iglée42 <igleev2@gmail.com>
2023-08-18 11:35:06 +0200

    Trying to fix glass shaft

---
+++ src/main/java/fr/iglee42/createcasing/events/ModEventClientBusEvents.java
@@ -0,0 +1,23 @@
+package fr.iglee42.createcasing.events;
+
+import fr.iglee42.createcasing.registries.ModBlocks;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+import static fr.iglee42.createcasing.CreateCasing.MODID;
+
+@Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ModEventClientBusEvents {
+
+    @SubscribeEvent
+    public static void clientSetup(final FMLClientSetupEvent event) {
+
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.GLASS_SHAFT.get(), RenderType.cutoutMipped());
+
+    }
+
+}
