9ca1061389b76fffb5adc6c7b44872bc0c57a34c
iglée42 <igleev2@gmail.com>
2023-08-31 12:47:31 +0200

    Some Fixs

--- src/main/java/fr/iglee42/createcasing/registries/ModBlocks.java
+++ src/main/java/fr/iglee42/createcasing/registries/ModBlocks.java
@@ -117,7 +117,7 @@ public class ModBlocks {
             .initialProperties(SharedProperties::copperMetal)
             .properties(p -> p.color(MaterialColor.TERRACOTTA_LIGHT_GRAY))
             .properties(BlockBehaviour.Properties::noOcclusion)
-            .transform(axeOrPickaxe())
+            .transform(pickaxeOnly())
             .onRegister(CreateRegistrate.blockModel(() -> PipeAttachmentModel::new))
             .loot((p, b) -> p.dropOther(b, AllBlocks.FLUID_PIPE.get()))
             .transform(EncasingRegistry.addVariantTo(AllBlocks.FLUID_PIPE))
@@ -330,6 +330,7 @@ public class ModBlocks {
                             .transform(BuilderTransformers.encasedShaft(casing, () -> sprite))
                             .transform(EncasingRegistry.addVariantTo(shaft))
                             .transform(axeOrPickaxe())
+                            .loot((l,s)->l.dropOther(s,s.getShaft().get().asItem()))
                             .register();
                 } catch (NoSuchFieldException | IllegalAccessException ex) {
                     ex.printStackTrace();
@@ -340,6 +341,7 @@ public class ModBlocks {
                     .transform(encasedNoSpriteShaft("industrial_iron"))
                     .transform(EncasingRegistry.addVariantTo(shaft))
                     .transform(axeOrPickaxe())
+                    .loot((l,s)->l.dropOther(s,s.getShaft().get().asItem()))
                     .register();
         });
     }
