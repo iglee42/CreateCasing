d5dd68172c12a64bcb626bd7cb00b5f16d936637
iglée42 <igleev2@gmail.com>
2023-09-12 21:56:17 +0200

    Things for api

---
+++ src/main/java/fr/iglee42/createcasing/items/ApiVerticalGearboxItem.java
@@ -0,0 +1,59 @@
+package fr.iglee42.createcasing.items;
+
+import com.simibubi.create.content.kinetics.base.IRotate;
+import com.simibubi.create.foundation.utility.Iterate;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.Direction.Axis;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.Map;
+
+public class ApiVerticalGearboxItem extends BlockItem {
+
+	public ApiVerticalGearboxItem(Properties builder, Block block) {
+		super(block, builder);
+	}
+
+	@Override
+	public String getDescriptionId() {
+		return "item.createcasing.vertical_" + ForgeRegistries.BLOCKS.getKey(getBlock()).getPath();
+	}
+
+	@Override
+	public void registerBlocks(Map<Block, Item> p_195946_1_, Item p_195946_2_) {
+	}
+
+	@Override
+	protected boolean updateCustomBlockEntityTag(BlockPos pos, Level world, Player player, ItemStack stack, BlockState state) {
+		Axis prefferedAxis = null;
+		for (Direction side : Iterate.horizontalDirections) {
+			BlockState blockState = world.getBlockState(pos.relative(side,1));
+			if (blockState.getBlock() instanceof IRotate) {
+				if (((IRotate) blockState.getBlock()).hasShaftTowards(world, pos.relative(side,1), blockState,
+						side.getOpposite()))
+					if (prefferedAxis != null && prefferedAxis != side.getAxis()) {
+						prefferedAxis = null;
+						break;
+					} else {
+						prefferedAxis = side.getAxis();
+					}
+			}
+		}
+
+		Axis axis = prefferedAxis == null ? player.getDirection()
+				.getClockWise()
+				.getAxis() : prefferedAxis == Axis.X ? Axis.Z : Axis.X;
+		world.setBlockAndUpdate(pos, state.setValue(BlockStateProperties.AXIS, axis));
+		return super.updateCustomBlockEntityTag(pos, world, player, stack, state);
+	}
+
+}
---
+++ src/main/java/fr/iglee42/createcasing/utils/Deferred.java
@@ -0,0 +1,9 @@
+package fr.iglee42.createcasing.utils;
+
+public class Deferred<T> {
+    public T data;
+
+    public void set(T val) {
+        data = val;
+    }
+}
