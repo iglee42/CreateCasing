ce7e71735173edc547e62c03637265a6934d0fe6
iglée42 <igleev2@gmail.com>
2023-09-12 21:56:05 +0200

    Starting to implement Metal Shaft

---
+++ src/main/java/fr/iglee42/createcasing/blockEntities/MetalShaftBlockEntity.java
@@ -0,0 +1,29 @@
+package fr.iglee42.createcasing.blockEntities;
+
+import com.simibubi.create.content.kinetics.simpleRelays.BracketedKineticBlockEntity;
+import fr.iglee42.createcasing.registries.ModBlocks;
+import fr.iglee42.createcasing.registries.ModSounds;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Vec3i;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class MetalShaftBlockEntity extends BracketedKineticBlockEntity {
+    public MetalShaftBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+        super(type, pos, state);
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+
+        if (ModBlocks.MLDEG_SHAFT.has(getBlockState())){
+            if (getSpeed() == 256 || getSpeed() == -256){
+                if (lazyTickCounter % 60 == 0)ModSounds.MLDEG.playAt(level,worldPosition,0.25f,0.5f,false);
+            }
+        }
+
+    }
+
+
+}
