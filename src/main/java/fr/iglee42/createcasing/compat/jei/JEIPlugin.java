58c6ae98f2173544d067184d9c989e23fbf5de81
iglée42 <igleev2@gmail.com>
2023-09-13 11:52:07 +0200

    Adding Jei Compat

---
+++ src/main/java/fr/iglee42/createcasing/compat/jei/JEIPlugin.java
@@ -0,0 +1,29 @@
+package fr.iglee42.createcasing.compat.jei;
+
+import fr.iglee42.createcasing.CreateCasing;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.registration.IModIngredientRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@JeiPlugin
+public class JEIPlugin implements IModPlugin {
+    @Override
+    public ResourceLocation getPluginUid() {
+        return new ResourceLocation("createcasing:jei");
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        IModPlugin.super.registerRecipes(registration);
+        List<ItemStack> stacks = new ArrayList<>();
+        CreateCasing.hidedItems.forEach(i->stacks.add(new ItemStack(i)));
+        registration.getIngredientManager().removeIngredientsAtRuntime(VanillaTypes.ITEM_STACK,stacks);
+    }
+}
