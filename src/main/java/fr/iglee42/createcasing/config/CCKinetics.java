f87adf446815ff4d40afa6fde1f7a7c0f9b5f14b
iglée42 <igleev2@gmail.com>
2023-10-11 14:25:45 +0200

    Trying to add stress values to config

--- src/main/java/fr/iglee42/createcasing/config/CCKinetics.java
+++ src/main/java/fr/iglee42/createcasing/config/CCKinetics.java
@@ -1,8 +1,8 @@
 package fr.iglee42.createcasing.config;
 
 import com.simibubi.create.foundation.config.ConfigBase;
-import com.simibubi.create.infrastructure.config.AllConfigs;
-import com.simibubi.create.infrastructure.config.CKinetics;
+import com.simibubi.create.infrastructure.config.CStress;
+
 
 public class CCKinetics extends ConfigBase {
 
@@ -12,7 +12,7 @@ public class CCKinetics extends ConfigBase {
     public ConfigBool shouldGlassShaftBreak = b(true,"shouldGlassShaftBreak", Comments.shouldGlassShaftBreak);
 
     public ConfigInt maxSpeedWoodenShaft = i(32,2, 256,"maxSpeedWoodenShaft",Comments.maxSpeedWoodenShaft);
-
+    //public final CStress stressValues = nested(1, CStress::new, Comments.stress);
 
     @Override
     public String getName() {
@@ -24,5 +24,7 @@ public class CCKinetics extends ConfigBase {
         static String shouldWoodenShaftBreak = "Should Wooden Shaft break if the speed is too high";
         static String shouldGlassShaftBreak = "Should Glass Shaft break if the system is overstressed";
         static String maxSpeedWoodenShaft = "The max speed wooden shafts can endure";
+        static String stress = "Fine tune the kinetic stats of individual components";
+
     }
 }
