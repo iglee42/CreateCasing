c98a52a9f9ce306aa725a8fe98fe1650ec311e6c
iglée42 <igleev2@gmail.com>
2023-10-11 14:25:02 +0200

    Mixin for Custom Depots

---
+++ src/main/java/fr/iglee42/createcasing/mixins/create/AssemblyOperatorBlockItemMixin.java
@@ -0,0 +1,18 @@
+package fr.iglee42.createcasing.mixins.create;
+
+import com.simibubi.create.content.processing.AssemblyOperatorBlockItem;
+import fr.iglee42.createcasing.blocks.customs.CustomDepotBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = AssemblyOperatorBlockItem.class,remap = false)
+public class AssemblyOperatorBlockItemMixin {
+
+    @Inject(method = "operatesOn",at = @At("HEAD"),cancellable = true)
+    private void inject(BlockState placedOnState, CallbackInfoReturnable<Boolean> cir){
+        if (placedOnState.getBlock() instanceof CustomDepotBlock) cir.setReturnValue(true);
+    }
+}
