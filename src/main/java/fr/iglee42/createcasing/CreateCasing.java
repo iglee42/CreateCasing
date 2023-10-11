6091a925d66b8fb901c8caf71e112d0a98155a81
iglée42 <igleev2@gmail.com>
2023-10-11 14:30:42 +0200

    Main Class & Gradle

--- src/main/java/fr/iglee42/createcasing/CreateCasing.java
+++ src/main/java/fr/iglee42/createcasing/CreateCasing.java
@@ -9,6 +9,7 @@ import com.simibubi.create.foundation.item.KineticStats;
 import com.simibubi.create.foundation.item.TooltipHelper;
 import com.simibubi.create.foundation.item.TooltipModifier;
 import fr.iglee42.createcasing.compat.createcrystalclear.CreateCrystalClearCompatibility;
+import fr.iglee42.createcasing.compat.kubejs.KubeJSCompatInit;
 import fr.iglee42.createcasing.config.ModConfigs;
 import fr.iglee42.createcasing.registries.ModBlockEntities;
 import fr.iglee42.createcasing.registries.ModBlocks;
@@ -75,13 +76,18 @@ public class CreateCasing {
         ModSounds.prepare();
         ModBlocks.register();
         ModBlockEntities.register();
+        ModPackets.registerPackets();
+
+        if (ModList.get().isLoaded("kubejs")) {
+            KubeJSCompatInit.init();
+        }
 
         if (isCrystalClearLoaded()) CreateCrystalClearCompatibility.register();
 
         DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> CreateCasingClient.onCtorClient(modEventBus, forgeEventBus));
 
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        forgeEventBus.addListener(this::onPlayerInteract);
+        forgeEventBus.addListener(this::onPlayerRightClickOnBlock);
 
         modEventBus.addListener(ModSounds::register);
 
@@ -108,7 +114,7 @@ public class CreateCasing {
         return ModList.get().isLoaded("create_crystal_clear");
     }
 
-    private void onPlayerInteract(PlayerInteractEvent.RightClickBlock event){
+    private void onPlayerRightClickOnBlock(PlayerInteractEvent.RightClickBlock event){
         Level world = event.getEntity().getLevel();
         if (event.getItemStack().isEmpty()) return;
         if (AllBlocks.MECHANICAL_PRESS.has(world.getBlockState(event.getPos()))){
