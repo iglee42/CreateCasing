6f19b5222f738d9c458f097ab9c0cae777c0bfda
iglée42 <igleev2@gmail.com>
2023-11-08 22:09:17 +0100

    Adding Custom Depot Interaction with mechanical arm (Issue #61)

---
+++ src/main/java/fr/iglee42/createcasing/mixins/create/AllArmInteractionPointTypesMixin.java
@@ -0,0 +1,22 @@
+package fr.iglee42.createcasing.mixins.create;
+
+import com.simibubi.create.content.kinetics.mechanicalArm.AllArmInteractionPointTypes;
+import fr.iglee42.createcasing.blocks.api.ApiDepotBlock;
+import fr.iglee42.createcasing.registries.ModBlocks;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = AllArmInteractionPointTypes.DepotType.class,remap = false)
+public class AllArmInteractionPointTypesMixin {
+
+    @Inject(method = "canCreatePoint", at = @At("HEAD"), cancellable = true)
+    private void inject(Level level, BlockPos pos, BlockState state, CallbackInfoReturnable<Boolean> cir){
+        if (ModBlocks.BRASS_DEPOT.has(state) || ModBlocks.COPPER_DEPOT.has(state) || ModBlocks.RAILWAY_DEPOT.has(state) || ModBlocks.INDUSTRIAL_IRON_DEPOT.has(state) || state.getBlock() instanceof ApiDepotBlock) cir.setReturnValue(true);
+    }
+
+}
