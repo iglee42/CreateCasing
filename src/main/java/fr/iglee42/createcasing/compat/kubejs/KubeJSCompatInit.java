42fe2b77b2e496b39e04bdc4b60a4dbba045970f
iglée42 <igleev2@gmail.com>
2023-11-18 18:01:55 +0100

    Fix Kubejs plugin

--- src/main/java/fr/iglee42/createcasing/compat/kubejs/KubeJSCompatInit.java
+++ src/main/java/fr/iglee42/createcasing/compat/kubejs/KubeJSCompatInit.java
@@ -1,21 +1,25 @@
 package fr.iglee42.createcasing.compat.kubejs;
 
-import dev.latvian.mods.kubejs.script.ScriptType;
+import fr.iglee42.createcasing.CreateCasing;
 import fr.iglee42.createcasing.compat.kubejs.casing.CasingEventJs;
+import fr.iglee42.createcasing.compat.kubejs.depot.DepotEventJs;
 import fr.iglee42.createcasing.compat.kubejs.encased.EncasedEventJs;
 import fr.iglee42.createcasing.compat.kubejs.gearbox.GearboxEventJs;
+import fr.iglee42.createcasing.compat.kubejs.mixer.MixerEventJs;
+import fr.iglee42.createcasing.compat.kubejs.press.PressEventJs;
 import net.minecraftforge.fml.ModList;
 
 public class KubeJSCompatInit {
 
     public static void init(){
         if(ModList.get().isLoaded("kubejs")){
+            CreateCasing.LOGGER.info("Event posting...");
             CreateCasingEventsJS.CASING.post(new CasingEventJs());
             CreateCasingEventsJS.GEARBOX.post(new GearboxEventJs());
             CreateCasingEventsJS.ENCASED.post(new EncasedEventJs());
-            CreateCasingEventsJS.DEPOT.post(new EncasedEventJs());
-            CreateCasingEventsJS.MIXER.post(new EncasedEventJs());
-            CreateCasingEventsJS.PRESS.post(new EncasedEventJs());
+            CreateCasingEventsJS.DEPOT.post(new DepotEventJs());
+            CreateCasingEventsJS.MIXER.post(new MixerEventJs());
+            CreateCasingEventsJS.PRESS.post(new PressEventJs());
         }
     }
 }
