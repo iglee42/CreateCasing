f6e6ebb78995a0be90168869f8c3feb5afa7e4e5
iglée42 <igleev2@gmail.com>
2023-11-20 21:19:26 +0100

    Fix Kubejs plugin

--- src/main/java/fr/iglee42/createcasing/compat/kubejs/KubeJSCompatInit.java
+++ src/main/java/fr/iglee42/createcasing/compat/kubejs/KubeJSCompatInit.java
@@ -1,5 +1,6 @@
 package fr.iglee42.createcasing.compat.kubejs;
 
+import dev.latvian.mods.kubejs.script.ScriptType;
 import fr.iglee42.createcasing.CreateCasing;
 import fr.iglee42.createcasing.compat.kubejs.casing.CasingEventJs;
 import fr.iglee42.createcasing.compat.kubejs.depot.DepotEventJs;
@@ -14,12 +15,12 @@ public class KubeJSCompatInit {
     public static void init(){
         if(ModList.get().isLoaded("kubejs")){
             CreateCasing.LOGGER.info("Event posting...");
-            CreateCasingEventsJS.CASING.post(new CasingEventJs());
-            CreateCasingEventsJS.GEARBOX.post(new GearboxEventJs());
-            CreateCasingEventsJS.ENCASED.post(new EncasedEventJs());
-            CreateCasingEventsJS.DEPOT.post(new DepotEventJs());
-            CreateCasingEventsJS.MIXER.post(new MixerEventJs());
-            CreateCasingEventsJS.PRESS.post(new PressEventJs());
+            CreateCasingEventsJS.CASING.post(ScriptType.STARTUP,new CasingEventJs());
+            CreateCasingEventsJS.GEARBOX.post(ScriptType.STARTUP,new GearboxEventJs());
+            CreateCasingEventsJS.ENCASED.post(ScriptType.STARTUP,new EncasedEventJs());
+            CreateCasingEventsJS.DEPOT.post(ScriptType.STARTUP,new DepotEventJs());
+            CreateCasingEventsJS.MIXER.post(ScriptType.STARTUP,new MixerEventJs());
+            CreateCasingEventsJS.PRESS.post(ScriptType.STARTUP,new PressEventJs());
         }
     }
 }
