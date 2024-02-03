d187c9e67dd937016a074b258f5f63c365f9d826
iglée42 <igleev2@gmail.com>
2024-02-03 22:33:30 +0100

    Main files changes

--- src/main/java/fr/iglee42/createcasing/CreateCasing.java
+++ src/main/java/fr/iglee42/createcasing/CreateCasing.java
@@ -138,6 +138,10 @@ public class CreateCasing {
                 world.setBlockAndUpdate(event.getPos(), ModBlocks.INDUSTRIAL_IRON_PRESS.getDefaultState().setValue(HORIZONTAL_FACING, facing));
                 event.setCancellationResult(InteractionResult.SUCCESS);
                 event.setCanceled(true);
+            } else if (event.getItemStack().is(ModBlocks.CREATIVE_CASING.get().asItem())){
+                world.setBlockAndUpdate(event.getPos(), ModBlocks.CREATIVE_PRESS.getDefaultState().setValue(HORIZONTAL_FACING, facing));
+                event.setCancellationResult(InteractionResult.SUCCESS);
+                event.setCanceled(true);
             }
         } else if (AllBlocks.MECHANICAL_MIXER.has(world.getBlockState(event.getPos()))){
             if (event.getItemStack().is(AllBlocks.BRASS_CASING.get().asItem())){
@@ -156,6 +160,10 @@ public class CreateCasing {
                 world.setBlockAndUpdate(event.getPos(), ModBlocks.INDUSTRIAL_IRON_MIXER.getDefaultState());
                 event.setCancellationResult(InteractionResult.SUCCESS);
                 event.setCanceled(true);
+            } else if (event.getItemStack().is(ModBlocks.CREATIVE_CASING.get().asItem())){
+                world.setBlockAndUpdate(event.getPos(), ModBlocks.CREATIVE_MIXER.getDefaultState());
+                event.setCancellationResult(InteractionResult.SUCCESS);
+                event.setCanceled(true);
             }
         } else if (AllBlocks.DEPOT.has(world.getBlockState(event.getPos()))){
             if (event.getItemStack().is(AllBlocks.BRASS_CASING.get().asItem())){
@@ -172,6 +180,10 @@ public class CreateCasing {
                 event.setCanceled(true);
             } else if (event.getItemStack().is(AllBlocks.INDUSTRIAL_IRON_BLOCK.get().asItem())){
                 world.setBlockAndUpdate(event.getPos(), ModBlocks.INDUSTRIAL_IRON_DEPOT.getDefaultState());
+                event.setCancellationResult(InteractionResult.SUCCESS);
+                event.setCanceled(true);
+            } else if (event.getItemStack().is(ModBlocks.CREATIVE_CASING.get().asItem())){
+                world.setBlockAndUpdate(event.getPos(), ModBlocks.CREATIVE_DEPOT.getDefaultState());
                 event.setCancellationResult(InteractionResult.SUCCESS);
                 event.setCanceled(true);
             }
