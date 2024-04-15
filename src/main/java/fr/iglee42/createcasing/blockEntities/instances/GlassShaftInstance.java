65f9a9373ee97a16d57fbe9e612d3fefb932523b
iglée42 <igleev2@gmail.com>
2024-04-15 15:10:46 +0200

    Fix Glass Shaft transparency
    (cherry picked from commit 19f8e80fae406165fff8bb075cf836c56c6fc3a1)
    (cherry picked from commit e99cc7b8b0827e0c5bfb46a5d5acbc0678346c8a)

---
+++ src/main/java/fr/iglee42/createcasing/blockEntities/instances/GlassShaftInstance.java
@@ -0,0 +1,86 @@
+package fr.iglee42.createcasing.blockEntities.instances;
+
+import com.jozufozu.flywheel.api.Instancer;
+import com.jozufozu.flywheel.api.MaterialManager;
+import com.jozufozu.flywheel.util.transform.TransformStack;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
+import com.simibubi.create.AllPartialModels;
+import com.simibubi.create.content.kinetics.base.KineticBlockEntityRenderer;
+import com.simibubi.create.content.kinetics.base.SingleRotatingInstance;
+import com.simibubi.create.content.kinetics.base.flwdata.RotatingData;
+
+import com.simibubi.create.content.kinetics.simpleRelays.ICogWheel;
+import com.simibubi.create.foundation.render.AllMaterialSpecs;
+import fr.iglee42.createcasing.blockEntities.GlassShaftBlockEntity;
+import fr.iglee42.createcasing.blockEntities.renderers.GlassShaftRenderer;
+import fr.iglee42.createcasing.registries.ModPartialModels;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.Direction.AxisDirection;
+
+public class GlassShaftInstance extends SingleRotatingInstance<GlassShaftBlockEntity> {
+
+	protected RotatingData additionalShaft;
+
+	public GlassShaftInstance(MaterialManager materialManager, GlassShaftBlockEntity blockEntity) {
+		super(materialManager, blockEntity);
+	}
+
+	@Override
+	public void init() {
+		super.init();
+
+		float speed = blockEntity.getSpeed();
+		Direction.Axis axis = KineticBlockEntityRenderer.getRotationAxisOf(blockEntity);
+		BlockPos pos = blockEntity.getBlockPos();
+		float offset = GlassShaftRenderer.getShaftAngleOffset(axis, pos);
+		Direction facing = Direction.fromAxisAndDirection(axis, AxisDirection.POSITIVE);
+		Instancer<RotatingData> half = materialManager.cutout(RenderType.cutoutMipped()).material(AllMaterialSpecs.ROTATING).getModel(ModPartialModels.GLASS_SHAFT, blockState,
+			facing, () -> this.rotateToAxis(axis));
+
+		additionalShaft = setup(half.createInstance(), speed);
+		additionalShaft.setRotationOffset(offset);
+	}
+
+	@Override
+	protected Instancer<RotatingData> getModel() {
+		return super.getModel();
+	}
+
+	private PoseStack rotateToAxis(Direction.Axis axis) {
+		Direction facing = Direction.fromAxisAndDirection(axis, AxisDirection.POSITIVE);
+		PoseStack poseStack = new PoseStack();
+		TransformStack.cast(poseStack)
+				.centre()
+				.rotateToFace(facing)
+				.multiply(Axis.XN.rotationDegrees(-90))
+				.unCentre();
+		return poseStack;
+	}
+
+	@Override
+	public void update() {
+		super.update();
+		if (additionalShaft != null) {
+			updateRotation(additionalShaft);
+			additionalShaft.setRotationOffset(GlassShaftRenderer.getShaftAngleOffset(axis, pos));
+		}
+	}
+
+	@Override
+	public void updateLight() {
+		super.updateLight();
+		if (additionalShaft != null)
+			relight(pos, additionalShaft);
+	}
+
+	@Override
+	public void remove() {
+		super.remove();
+		if (additionalShaft != null)
+			additionalShaft.delete();
+	}
+
+}
