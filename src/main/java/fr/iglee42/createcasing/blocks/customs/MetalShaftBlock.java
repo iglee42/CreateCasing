ae8eacbba7e8f38b6a7ef3de0a119c77de73fad0
iglée42 <igleev2@gmail.com>
2024-04-15 15:11:12 +0200

    Update placement helper on shafts

--- src/main/java/fr/iglee42/createcasing/blocks/customs/MetalShaftBlock.java
+++ src/main/java/fr/iglee42/createcasing/blocks/customs/MetalShaftBlock.java
@@ -5,6 +5,8 @@ import com.simibubi.create.content.kinetics.simpleRelays.ShaftBlock;
 import com.simibubi.create.content.trains.station.AssemblyScreen;
 import com.simibubi.create.content.trains.station.StationScreen;
 import com.simibubi.create.foundation.gui.ScreenOpener;
+import com.simibubi.create.foundation.placement.IPlacementHelper;
+import com.simibubi.create.foundation.placement.PlacementHelpers;
 import fr.iglee42.createcasing.blockEntities.BrassShaftBlockEntity;
 import fr.iglee42.createcasing.blockEntities.MetalShaftBlockEntity;
 import fr.iglee42.createcasing.registries.ModBlockEntities;
@@ -15,7 +17,9 @@ import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -34,5 +38,13 @@ public class MetalShaftBlock extends ShaftBlock {
         return ModBlockEntities.METAL_SHAFT.get();
     }
 
-
+    @Override
+    public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult ray) {
+        IPlacementHelper helper = PlacementHelpers.get(WoodenShaftBlock.placementHelperId);
+        ItemStack heldItem = player.getItemInHand(hand);
+        if (helper.matchesItem(heldItem))
+            return helper.getOffset(player, world, state, pos, ray)
+                    .placeInWorld(world, (BlockItem) heldItem.getItem(), player, hand, ray);
+        return super.use(state, world, pos, player, hand, ray);
+    }
 }
