1dbef33899a9b57479d74ee4a59f6922d89680d2
iglée42 <igleev2@gmail.com>
2024-04-15 15:29:06 +0200

    Adding Easter eggs files
    (cherry picked from commit a64bfc2e00a89c0e4c4b4538366ff8f12cda063b)
    (cherry picked from commit c8f2a4c0fc11bcef2383d8ba148b1d799a6d0eac)

---
+++ src/main/java/fr/iglee42/createcasing/blocks/customs/MLDEGShaftBlock.java
@@ -0,0 +1,8 @@
+package fr.iglee42.createcasing.blocks.customs;
+
+public class MLDEGShaftBlock extends MetalShaftBlock {
+    public MLDEGShaftBlock(Properties properties) {
+        super(properties);
+    }
+
+}
