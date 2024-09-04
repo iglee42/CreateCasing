663ed736f022b4d28cc53090e39d04190f08b480
iglée42 <igleev2@gmail.com>
2024-09-04 21:27:25 +0200

    Fix for Create H fix (#89)

--- src/main/java/fr/iglee42/createcasing/mixins/create/AssemblyOperatorBlockItemMixin.java
+++ src/main/java/fr/iglee42/createcasing/mixins/create/AssemblyOperatorBlockItemMixin.java
@@ -2,6 +2,8 @@ package fr.iglee42.createcasing.mixins.create;
 
 import com.simibubi.create.content.processing.AssemblyOperatorBlockItem;
 import fr.iglee42.createcasing.blocks.customs.CustomDepotBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class AssemblyOperatorBlockItemMixin {
 
     @Inject(method = "operatesOn",at = @At("HEAD"),cancellable = true)
-    private void inject(BlockState placedOnState, CallbackInfoReturnable<Boolean> cir){
+    private void inject(LevelReader world, BlockPos pos, BlockState placedOnState, CallbackInfoReturnable<Boolean> cir){
         if (placedOnState.getBlock() instanceof CustomDepotBlock) cir.setReturnValue(true);
     }
 }
