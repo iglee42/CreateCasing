7c55563edf7c9eaf57ff851daeb51bf6b99634a1
iglée42 <igleev2@gmail.com>
2025-10-28 11:23:32 +0100

    Do an optimisation on the block entity type mixin to only do the check if the original return value is false (#143)

--- src/main/java/fr/iglee42/createcasing/mixins/minecraft/BlockEntityTypeMixin.java
+++ src/main/java/fr/iglee42/createcasing/mixins/minecraft/BlockEntityTypeMixin.java
@@ -2,20 +2,29 @@ package fr.iglee42.createcasing.mixins.minecraft;
 
 import com.simibubi.create.AllBlockEntityTypes;
 import fr.iglee42.createcasing.registries.EncasedBlockEntities;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Set;
 
 @Mixin(value = BlockEntityType.class)
 public class BlockEntityTypeMixin {
 
 
+    @Shadow
+    @Final
+    private Set<Block> validBlocks;
+
     @Inject(method = "isValid",at = @At("HEAD"),cancellable = true,remap = false)
     private void isValid(BlockState state, CallbackInfoReturnable<Boolean> cir){
-        if (EncasedBlockEntities.ENCASED_SHAFT.isBound() &&( this.equals(EncasedBlockEntities.ENCASED_SHAFT.get()) ||
+        if (!this.validBlocks.contains(state.getBlock()) && EncasedBlockEntities.ENCASED_SHAFT.isBound() &&( this.equals(EncasedBlockEntities.ENCASED_SHAFT.get()) ||
                 this.equals(EncasedBlockEntities.ENCASED_COGWHEEL.get()) ||
                 this.equals(EncasedBlockEntities.ENCASED_COGWHEEL_LARGE.get()) ||
                 this.equals(EncasedBlockEntities.ENCASED_FLUID_PIPE.get()) ||
