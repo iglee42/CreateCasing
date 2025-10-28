ac2a0dea67fa57f2fb3a54853b36241470c28f83
iglée42 <igleev2@gmail.com>
2025-10-28 11:14:55 +0100

    Fix #151 issue with 6.0.7

--- src/main/java/fr/iglee42/createcasing/blockEntities/visuals/CustomMixerVisual.java
+++ src/main/java/fr/iglee42/createcasing/blockEntities/visuals/CustomMixerVisual.java
@@ -7,6 +7,7 @@ import com.simibubi.create.content.kinetics.base.SingleAxisRotatingVisual;
 import com.simibubi.create.content.kinetics.mixer.MechanicalMixerBlockEntity;
 import com.simibubi.create.foundation.render.AllInstanceTypes;
 import dev.engine_room.flywheel.api.instance.Instance;
+import dev.engine_room.flywheel.api.visual.DynamicVisual;
 import dev.engine_room.flywheel.api.visualization.VisualizationContext;
 import dev.engine_room.flywheel.lib.instance.InstanceTypes;
 import dev.engine_room.flywheel.lib.instance.OrientedInstance;
@@ -136,8 +137,8 @@ public class CustomMixerVisual extends SingleAxisRotatingVisual<MechanicalMixerB
 		consumer.accept(mixerPole);
 	}
 
-	@Override
-	public void beginFrame(Context context) {
-		animate(context.partialTick());
-	}
+    @Override
+    public void beginFrame(DynamicVisual.Context ctx) {
+        animate(ctx.partialTick());
+    }
 }
