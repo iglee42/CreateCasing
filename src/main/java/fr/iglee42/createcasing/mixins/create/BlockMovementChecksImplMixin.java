d1dd31129a1112b97c3b1b20fd20988ee179b96d
iglée42 <igleev2@gmail.com>
2026-01-13 12:48:29 +0100

    Fix #166 (Ungluable blocks)
    (cherry picked from commit e0253bb32d865f92c7fe5944b2da63f5094019d0)

--- src/main/java/fr/iglee42/createcasing/mixins/create/BlockMovementChecksImplMixin.java
+++ src/main/java/fr/iglee42/createcasing/mixins/create/BlockMovementChecksImplMixin.java
@@ -7,6 +7,7 @@ import com.simibubi.create.content.kinetics.saw.SawBlock;
 import com.simibubi.create.impl.contraption.BlockMovementChecksImpl;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,22 +19,17 @@ public class BlockMovementChecksImplMixin {
     @Inject(method = "isNotSupportiveFallback",at=@At("HEAD"),cancellable = true)
     private static void encased$updateFallbackForCustomEncasedBlocks(BlockState state, Direction facing, CallbackInfoReturnable<Boolean> cir){
         if(state.getBlock() instanceof PortableStorageInterfaceBlock){
-            cir.setReturnValue(true);
+            cir.setReturnValue(state.getValue(PortableStorageInterfaceBlock.FACING) == facing);
             return;
         }
 
         if(state.getBlock() instanceof SawBlock){
-            cir.setReturnValue(true);
-            return;
-        }
-
-        if(state.getBlock() instanceof RollerBlock){
-            cir.setReturnValue(true);
+            cir.setReturnValue(state.getValue(BlockStateProperties.FACING) == facing);
             return;
         }
 
         if(state.getBlock() instanceof DrillBlock){
-            cir.setReturnValue(true);
+            cir.setReturnValue(state.getValue(BlockStateProperties.FACING) == facing);
             return;
         }
     }
