ff48bf7e7ef2d1749e1d38ea6d4266821db1d0c5
iglée42 <igleev2@gmail.com>
2026-05-16 11:46:14 +0200

    Reworked logic of Custom Encased Chain Drive (Removed Mixin of RotationPropagator) (#182)

--- src/main/java/fr/iglee42/createcasing/blocks/customs/CustomChainDriveBlock.java
+++ src/main/java/fr/iglee42/createcasing/blocks/customs/CustomChainDriveBlock.java
@@ -25,67 +25,6 @@ public class CustomChainDriveBlock extends ChainDriveBlock {
     }
 
     @Override
-    public BlockState updateShape(BlockState stateIn, Direction face, BlockState neighbour, LevelAccessor worldIn,
-                                  BlockPos currentPos, BlockPos facingPos) {
-        if ((neighbour.getBlock() instanceof CustomChainDriveBlock b && type.equals(b.getType())) || neighbour.isAir()) {
-
-            ChainDriveBlock.Part part = stateIn.getValue(PART);
-            Axis axis = stateIn.getValue(AXIS);
-            boolean connectionAlongFirst = stateIn.getValue(CONNECTED_ALONG_FIRST_COORDINATE);
-            Axis connectionAxis =
-                    connectionAlongFirst ? (axis == Axis.X ? Axis.Y : Axis.X) : (axis == Axis.Z ? Axis.Y : Axis.Z);
-
-            Axis faceAxis = face.getAxis();
-            boolean facingAlongFirst = axis == Axis.X ? faceAxis.isVertical() : faceAxis == Axis.X;
-            boolean positive = face.getAxisDirection() == AxisDirection.POSITIVE;
-
-            if (axis == faceAxis)
-                return stateIn;
-
-            if (!(neighbour.getBlock() instanceof CustomChainDriveBlock)) {
-                if (facingAlongFirst != connectionAlongFirst || part == ChainDriveBlock.Part.NONE)
-                    return stateIn;
-                if (part == ChainDriveBlock.Part.MIDDLE)
-                    return stateIn.setValue(PART, positive ? ChainDriveBlock.Part.END : ChainDriveBlock.Part.START);
-                if ((part == ChainDriveBlock.Part.START) == positive)
-                    return stateIn.setValue(PART, ChainDriveBlock.Part.NONE);
-                return stateIn;
-            }
-
-            ChainDriveBlock.Part otherPart = neighbour.getValue(PART);
-            Axis otherAxis = neighbour.getValue(AXIS);
-            boolean otherConnection = neighbour.getValue(CONNECTED_ALONG_FIRST_COORDINATE);
-            Axis otherConnectionAxis =
-                    otherConnection ? (otherAxis == Axis.X ? Axis.Y : Axis.X) : (otherAxis == Axis.Z ? Axis.Y : Axis.Z);
-
-            if (neighbour.getValue(AXIS) == faceAxis)
-                return stateIn;
-            if (otherPart != ChainDriveBlock.Part.NONE && otherConnectionAxis != faceAxis)
-                return stateIn;
-
-            if (part == ChainDriveBlock.Part.NONE) {
-                part = positive ? ChainDriveBlock.Part.START : ChainDriveBlock.Part.END;
-                connectionAlongFirst = axis == Axis.X ? faceAxis.isVertical() : faceAxis == Axis.X;
-            } else if (connectionAxis != faceAxis) {
-                return stateIn;
-            }
-
-            if ((part == ChainDriveBlock.Part.START) != positive)
-                part = ChainDriveBlock.Part.MIDDLE;
-
-            return stateIn.setValue(PART, part)
-                    .setValue(CONNECTED_ALONG_FIRST_COORDINATE, connectionAlongFirst);
-        }
-        return stateIn;
-    }
-
-    public static boolean areCustomBlocksConnected(BlockState state, BlockState other, Direction facing) {
-        if (!(state.getBlock() instanceof CustomChainDriveBlock b && other.getBlock() instanceof CustomChainDriveBlock b1 && b.getType().equals(b1.getType()))) return false;
-        return areBlocksConnected(state, other, facing);
-    }
-
-
-    @Override
     public BlockEntityType<? extends KineticBlockEntity> getBlockEntityType() {
         return EncasedBlockEntities.CHAIN_DRIVE.get();
     }
--- src/main/java/fr/iglee42/createcasing/mixins/create/ChainDriveBlockMixin.java
+++ src/main/java/fr/iglee42/createcasing/mixins/create/ChainDriveBlockMixin.java
@@ -5,20 +5,137 @@ import fr.iglee42.createcasing.blocks.customs.CustomChainDriveBlock;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = ChainDriveBlock.class,remap = false)
+@Mixin(value = ChainDriveBlock.class, remap = false)
 public class ChainDriveBlockMixin {
 
-    @Inject(method = "updateShape",at=@At("HEAD"),cancellable = true)
-    private void encased$dontConnectToCustom(BlockState stateIn, Direction face, BlockState neighbour, LevelAccessor worldIn, BlockPos currentPos, BlockPos facingPos, CallbackInfoReturnable<BlockState> cir){
-        if (neighbour.getBlock() instanceof CustomChainDriveBlock) {
-            cir.setReturnValue(stateIn);
+    @Inject(method = "updateShape", at = @At("HEAD"), cancellable = true)
+    private void encased$dontConnectToInvalidCustom(
+            BlockState stateIn,
+            Direction face,
+            BlockState neighbour,
+            LevelAccessor worldIn,
+            BlockPos currentPos,
+            BlockPos facingPos,
+            CallbackInfoReturnable<BlockState> cir
+    ) {
+
+        Block self = stateIn.getBlock();
+        Block other = neighbour.getBlock();
+
+        boolean selfCustom = self instanceof CustomChainDriveBlock;
+        boolean otherCustom = other instanceof CustomChainDriveBlock;
+
+        /*
+         * Vanilla <-> Custom
+         *
+         * Disconnect the two
+         */
+        if (selfCustom != otherCustom) {
+            cir.setReturnValue(createCasing$disconnect(stateIn, face));
             return;
         }
+
+        /*
+         * Custom A <-> Custom B
+         * Different Types
+         */
+        if (selfCustom) {
+            CustomChainDriveBlock selfDrive = (CustomChainDriveBlock) self;
+            CustomChainDriveBlock otherDrive = (CustomChainDriveBlock) other;
+
+            if (!selfDrive.getType().equals(otherDrive.getType())) {
+                cir.setReturnValue(createCasing$disconnect(stateIn, face));
+            }
+        }
+    }
+
+    @Inject(method = "areBlocksConnected", at = @At("HEAD"), cancellable = true)
+    private static void encased$dontConnectCustom(
+            BlockState state,
+            BlockState other,
+            Direction facing,
+            CallbackInfoReturnable<Boolean> cir
+    ) {
+
+        boolean isCustom = state.getBlock() instanceof CustomChainDriveBlock;
+        boolean isOtherCustom = other.getBlock() instanceof CustomChainDriveBlock;
+
+        /*
+         * Vanilla <-> Custom
+         */
+        if (isCustom != isOtherCustom) {
+            cir.setReturnValue(false);
+            return;
+        }
+
+        /*
+         * Custom A <-> Custom B
+         */
+        if (isCustom) {
+            CustomChainDriveBlock chainDrive = (CustomChainDriveBlock) state.getBlock();
+            CustomChainDriveBlock otherDrive = (CustomChainDriveBlock) other.getBlock();
+
+            if (!chainDrive.getType().equals(otherDrive.getType())) {
+                cir.setReturnValue(false);
+            }
+        }
+    }
+
+    @Unique
+    private BlockState createCasing$disconnect(BlockState stateIn, Direction face) {
+
+        ChainDriveBlock.Part part = stateIn.getValue(ChainDriveBlock.PART);
+        Direction.Axis axis = stateIn.getValue(ChainDriveBlock.AXIS);
+
+        boolean connectionAlongFirst =
+                stateIn.getValue(ChainDriveBlock.CONNECTED_ALONG_FIRST_COORDINATE);
+
+        Direction.Axis faceAxis = face.getAxis();
+
+        boolean facingAlongFirst =
+                axis == Direction.Axis.X
+                        ? faceAxis.isVertical()
+                        : faceAxis == Direction.Axis.X;
+
+        boolean positive =
+                face.getAxisDirection() == Direction.AxisDirection.POSITIVE;
+
+        /*
+         * Same logic from vanilla Create
+         */
+
+        if (axis == faceAxis) {
+            return stateIn;
+        }
+
+        if (facingAlongFirst != connectionAlongFirst || part == ChainDriveBlock.Part.NONE) {
+            return stateIn;
+        }
+
+        if (part == ChainDriveBlock.Part.MIDDLE) {
+            return stateIn.setValue(
+                    ChainDriveBlock.PART,
+                    positive
+                            ? ChainDriveBlock.Part.END
+                            : ChainDriveBlock.Part.START
+            );
+        }
+
+        if ((part == ChainDriveBlock.Part.START) == positive) {
+            return stateIn.setValue(
+                    ChainDriveBlock.PART,
+                    ChainDriveBlock.Part.NONE
+            );
+        }
+
+        return stateIn;
     }
 }
